b3ba075f7772237d3cdcd920d0814f5dac670906
Marnie Reid <marnie@kodewithklossy.com>
2023-06-19 14:38:13 -0500

    Initial Commit

---
+++ ToDoList_Help/ToDoList_HelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_HelpApp.swift
+//  ToDoList_Help
+//
+//  Created by scholar on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_HelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
